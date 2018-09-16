892677e9e7485d5a0b27f3f40eee4d5990bfe5dd
lochg <loc_huynh@hitekgeek.com>
2018-09-16 14:17:10 +0700

    Add connect and disconnect event handlers

--- server/readme.txt
+++ server/readme.txt
@@ -1,1 +1,11 @@
 REM Source code Node.js
+
+git commit -m "Nội dung messages"
+
+Using heroku
+0. Tạo account (email, password)
+1. Install heroku
+2. command line: heroku login với  (email, password) ở 0.
+3. command line: git push heroku
+3. command line: heroku create (deploy)
+4. command line: heroku open (Mở trang web đã được deploy)
--- server/server.js
+++ server/server.js
@@ -1,16 +1,24 @@
 const path = require('path');
+const http = require('http');
 const express = require('express');
+const socketIO = require('socket.io');
+
+const publicPath = path.join(__dirname, '../public');
+const port = process.env.PORT || 3000;
 
 var app = express();
-
-var publicPath = path.join(__dirname, '../public');
-
-var port = process.env.PORT || 3000;
+var server = http.createServer(app);
+var io = socketIO(server);
 
 app.use(express.static(publicPath));
 
-console.log(publicPath);
+io.on('connection', (socket) => {
+    console.log('New user connected');
+    socket.on('disconnect', () => {
+        console.log('User was disconnected');
+    });
+})
 
-app.listen(port, () => {
+server.listen(port, () => {
     console.log(`Server is up on port ${port}`);
 });
